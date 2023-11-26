1e21f522cd006b70cfbd3ef256236cd1ab988d8d
Parth27904 <parth27092004@gmail.com>
2023-11-27 00:14:39 +0530

    UI Changed

--- lib/Register.dart
+++ lib/Register.dart
@@ -12,104 +12,76 @@ class _MyRegisterState extends State<MyRegister> {
   Widget build(BuildContext context) {
     return Scaffold(
       body:Container(
-        padding: const EdgeInsets.fromLTRB(10, 20, 10, 20),
-        color: Colors.black,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage("assets/backTech.jpg"),
+            fit: BoxFit.cover
+          )
+        ),
         child: Stack(
           children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              children: <Widget>[
-                Align(
-                  alignment: Alignment.topCenter,
-                  child: Image.asset('assets/cpc.jpeg',
-                  height: 350,
-                  width: 350,),
-                ),
-              ],
-            ),
             SingleChildScrollView(
               child: Container(
-                padding: EdgeInsets.only(
-                    top: MediaQuery.of(context).size.height*0.45,
-                    right: 35,
-                    left: 35),
-                child: Column(
-                  children: [
-                    TextField(
-                      decoration: InputDecoration(
-                        filled: true,
-                        fillColor: Colors.grey.shade100,
-                        hintText: 'Name',
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10)
-                        )
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.grey.shade100,
-                          hintText: 'Mobile No.',
-                          border: OutlineInputBorder(
+                height: 830,
+                width: 430,
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(10, 0, 10, 40),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                      image: DecorationImage(
+                          image: AssetImage("assets/back.png"),
+                      )
+                  ),
+                  child: Column(
+                    children: [
+                      SizedBox(height: 330,),
+                      Container(
+                        width: 300,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            filled: true,
+                            fillColor: Colors.grey.shade100,
+                            hintText: 'Name',
+                            border: OutlineInputBorder(
                               borderRadius: BorderRadius.circular(10)
-                          )
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.grey.shade100,
-                          hintText: 'State',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10)
-                          )
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 15,
-                    ),
-                    TextField(
-                      decoration: InputDecoration(
-                          filled: true,
-                          fillColor: Colors.grey.shade100,
-                          hintText: 'District',
-                          border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10)
-                          )
-                      ),
-                    ),
-                    const SizedBox(
-                      height: 40,
-                    ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        const Text('Sign UP',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 35,
-                            fontWeight: FontWeight.bold
+                            )
                           ),
                         ),
-                        CircleAvatar(
-                          radius: 30,
-                          backgroundColor: Colors.white,
-                          child: IconButton(
-                            color: Colors.black,
-                            onPressed: (){},
-                            icon : const Icon(Icons.arrow_forward),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Container(
+                        width: 300,
+                        child: TextField(
+                          decoration: InputDecoration(
+                              filled: true,
+                              fillColor: Colors.grey.shade100,
+                              hintText: 'Mobile No.',
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10)
+                              )
                           ),
-                        )
-                      ],
-                    )
-                  ],
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 15,
+                      ),
+
+                      const SizedBox(
+                        height: 20,
+                      ),
+                          CircleAvatar(
+                            radius: 30,
+                            backgroundColor: Colors.black,
+                            child: IconButton(
+                              color: Colors.white,
+                              onPressed: (){},
+                              icon : const Icon(Icons.arrow_forward),
+                            ),
+                          ),
+                    ],
+                  ),
                 ),
               ),
             )
